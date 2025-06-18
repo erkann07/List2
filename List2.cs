a0c26d0b31f87a12bd28fa81ca5073e0986500a0
Erkan <erkankrgz0707@gmail.com>
2025-06-18 15:18:41 +0300

    Add files via upload

---
+++ List2.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Lütfen 5 adet kahve ismi giriniz.");
+List<string> kahveListesi = new List<string>();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Kahve {i + 1}: ");
+    string kahve = Console.ReadLine();
+
+    // İlk harfi büyük, kalanları küçük yap
+    string duzenliIsim = char.ToUpper(kahve[0]) + kahve.Substring(1).ToLower();
+
+    // DÜZENLENMİŞ ismi listeye ekle
+    kahveListesi.Add(duzenliIsim);
+}
+
+Console.WriteLine("------- KAHVE LİSTENİZ -------");
+foreach (var item in kahveListesi)
+{
+    Console.WriteLine(item);
+}
